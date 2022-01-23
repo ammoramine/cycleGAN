9df8ab592c434ac9283f2241bfbb855571b80db8
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 20:36:19 +0100

    added tests to check if shape of image is the same through the dataset

--- tests/test_data_loader/test_datasets/test_dataset_full_transform.py
+++ tests/test_data_loader/test_datasets/test_dataset_full_transform.py
@@ -10,3 +10,12 @@ def test_elements_of_dataset_have_4_channel(dataset_full_transform):
     for el in dataset_full_transform:
         assert el[0].shape[0] == 4
 
+
+#TODO : resolve the problem of dataset with
+# different size of images, by proposing architectural
+# solution and easy solutions, with shape transformation
+def test_elements_have_the_same_shape(dataset_full_transform):
+    firstel = dataset_full_transform[0]
+    shape_first_rgba_image = firstel[0].shape
+    for el in dataset_full_transform:
+        assert el[0].shape == shape_first_rgba_image
