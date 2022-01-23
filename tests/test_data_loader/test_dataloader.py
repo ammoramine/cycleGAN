1ecc261b6b86ecf930440c832eac6db8a1fe1a10
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 21:05:07 +0100

    added test for dataloader, to check if input and output are of same type

--- tests/test_data_loader/test_dataloader.py
+++ tests/test_data_loader/test_dataloader.py
@@ -1,6 +1,7 @@
 from style_transfer.data_loader import dataloader_mod
 from style_transfer.data_loader import datasets
 import pytest
+import torch
 
 
 # @pytest.mark.parametrize("batch_size",32)
@@ -9,6 +10,13 @@ def data_loader():
     d_loader = dataloader_mod.get(batch_size=16)
     return d_loader
 
-def test_data_loader(data_loader):
+@pytest.mark.slow
+def test_data_loader_is_shuffled(data_loader):
+    firstel = [el for el in data_loader][0]
+    firstel_of_new_loading = [el for el in data_loader][0]
+    assert torch.any(firstel_of_new_loading[0] != firstel[0]) or torch.any(firstel_of_new_loading[1] != firstel[1])
+
+def test_dataloader_data_of_float_type(data_loader):
     for el in data_loader:
-        break
+        assert el[0].dtype == torch.float32
+        assert el[1].dtype == torch.float32
