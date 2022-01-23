a246a324a9394dbab919173f3d0330d7ea3175bf
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 14:06:36 +0100

    added test models

--- tests/test_models/test_generator_mod.py
+++ tests/test_models/test_generator_mod.py
@@ -1,25 +1,22 @@
 """
-testing the model
+    testing the model
 """
-import pytest
 from style_transfer import models
 
 
-def test_residual_block_shape_change(multiple_random_4D_tensor):
+def test_residual_block_shape_doesnt_change(random_4D_tensor):
     """
     the shape of the output of the residual block must
     be equal to its input.
     :param random_4D_tensor: fixture of 4D shape
     :return:
     """
-    print(f"\n checking for {len(multiple_random_4D_tensor)} number of tensors")
-    for random_4D_tensor in multiple_random_4D_tensor:
-        residual_block = models.residual_block_mod.ResidualBlock(4)
-        output = residual_block(random_4D_tensor)
-        assert output.shape == random_4D_tensor.shape
-        # print(output.shape)
+    print(f"\n checking for tensor of shape {random_4D_tensor.shape} ")
+    residual_block = models.residual_block_mod.ResidualBlock(4)
+    output = residual_block(random_4D_tensor)
+    # print(output.shape)
+    assert output.shape == random_4D_tensor.shape
 
-# @pytest.mark.skip()
 def test_if_shape_is_the_same(random_batch_one,model):
     # N = len(model.layers)
     print(f"batch size : {random_batch_one.shape}")
