a932710980b27873d8f3d671f02f048ff58bcb59
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 21:47:28 +0100

    added comment onn function

--- src/style_transfer/data_loader/dataloader_mod.py
+++ src/style_transfer/data_loader/dataloader_mod.py
@@ -2,7 +2,14 @@ from torch.utils.data import DataLoader
 from . import datasets
 
 def get(batch_size,index):
-    """dataloder"""
+    """
+        construct datalaoder with specific batch_size
+        for src ou target image (index=0 or 1), with shuffled data
+
+    :param batch_size: int
+    :param index: 0 or 1, if equal 0 dataloader or src image, otherwise of target iamge
+    :return:
+    """
     dataset = datasets.get(type_dataset="with_full_transform",index=index)
     dataloader = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True)
     return dataloader
