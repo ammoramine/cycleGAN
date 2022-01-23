d400dfeeca4a67422745f77dcf26ab368e262a85
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 15:50:47 +0100

    added fixture to simplify the test

--- tests/test_models/test_discriminator.py
+++ tests/test_models/test_discriminator.py
@@ -3,19 +3,29 @@
 """
 
 import torch
+import pytest
 
-def test_if_discriminator_output_shape_and_value(discriminator,random_batch):
+@pytest.fixture(scope="function")
+def discriminator_input_output(discriminator,random_batch):
+    outpt = discriminator(random_batch)
+    return (random_batch,outpt)
+
+def test_discriminator_output_shape(discriminator_input_output):
     """
     the shape of the output tensror should be two dimensional
-    of the form (N,1), where N is the size of the shape
-    the value must be between 0 and 1
-    :param discriminator:
+    of the form (N,1), where N is the size of the batch
+    :param discriminator_input_output:
     """
-    print(f"batch of shape {random_batch.shape}")
-    outpt = discriminator(random_batch)
-    print(f"shape of output {outpt.shape}")
-    assert outpt.shape[1] == 1
-    assert outpt.shape[0] == random_batch.shape[0]
-    # print(outpt)
+    inpt,outpt = discriminator_input_output
+    assert  outpt.shape[1] == 1
+    assert outpt.shape[0] == inpt.shape[0]
+
+
+def test_discriminator_output_value(discriminator_input_output):
+    """
+    it must be slower than 1
+    :return:
+    """
+    inpt,outpt = discriminator_input_output
     assert torch.min(outpt) > 0
     assert torch.max(outpt) < 1.0
