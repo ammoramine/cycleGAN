224326fff64f8b56a29c56b92444eb045a42b1ba
Amine AMMOR <aamineammor@gmail.com>
2022-01-23 19:02:21 +0100

    added script where to add the celeb dataset

---
+++ src/style_transfer/data_loader/celeb_dataloader.py
@@ -0,0 +1,8 @@
+#TODO: write code here for celeb dataset
+
+# from torchvision import datasets
+#
+# from data_path_accesor import
+#
+#
+# celeb_dataset = datasets.celeba.CelebA(root="Data",download=True,split="train")
