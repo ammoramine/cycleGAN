c30dc9ebfbe0d0afee29956ce9802ec72b77dc63
Amine AMMOR <aamineammor@gmail.com>
2022-01-24 17:14:02 +0100

    added TODO

---
+++ tests/test_models/test_parameters_variable.py
@@ -0,0 +1,7 @@
+"""
+    module to test if the parameters variable of the two generators and discriminators contains, effectively
+    all the parameters
+"""
+
+#TODO : fill these tests
+#TODO : fill these tests
